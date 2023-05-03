5ff72d6ab20528dd13d32d4d05212ef5d57b93ec
lekdup <lekdup@live.fr>
2023-05-03 14:52:13 +0200

    ajout de data

--- src/components/F0fimal/F0fimal.js
+++ src/components/F0fimal/F0fimal.js
@@ -1,27 +1,67 @@
 import { useState } from 'react';
 import './F0fimal.scss';
+import data from '../../data/data';
 
 const F0fimal = () => {
-    const [newPanda, setNewPanda] = useState('');
-
+    const [newPanda, setNewPanda] = useState({
+            id: '',
+            name: '',
+            image: ''
+        });
+    // console.log(newPanda);
+    console.log(data);
     return (
         <div className='content'>
             <h1>Les animaux</h1>
-            <div className="result">
-                <img src="https://images.theconversation.com/files/430483/original/file-20211105-9897-18ahqx2.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" className="result-image" alt="panda eating bamboo leaves"/>
-                <p className="result-name">Panda</p>
-                <button className='result-close'>x</button>
-            </div>
-            <form className='form'>
+            {data.map((animal) => (
+                <div className="result" key={animal.id}>
+                    <img 
+                        src={animal.image}
+                        className="result-image"
+                        alt={animal.name}
+                    />
+                    <p className="result-name">{animal.name}</p>
+                    <button className='result-close'>x</button>
+                </div>
+            ))}
+            <form
+                className='form'
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    setNewPanda({
+                        ...newPanda,
+                        data: [...data, {
+                            id: Math.max(...data.map((p) => p.id)) + 1,
+                            name: newPanda.name,
+                            image: newPanda.image,
+                        }]
+                    });
+                    // setNewPanda('');
+                }}
+            >
                 <input
                     type='text'
                     placeholder="Nom de l'animal"
                     className='form-name'
+                    value={newPanda.name}
+                    onChange={(e) => {
+                        setNewPanda({
+                            ...newPanda,
+                            name: e.target.value
+                        });
+                    }}
                 />
                 <input
                     type='url'
                     placeholder="Photo de l'animal (url)"
                     className='form-url'
+                    value={newPanda.image}
+                    onChange={(e) => {
+                        setNewPanda({
+                            ...newPanda,
+                            image: e.target.value
+                        })
+                    }}
                 />
                 <button
                     type='submit'
---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        id: 1,
+        name: "Panda",
+        image: "https://images.theconversation.com/files/430483/original/file-20211105-9897-18ahqx2.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
+    },
+];
